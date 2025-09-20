9e607163812a4209c55f846d8474b8619238991f
rajsekhar <rajsekhar9655@gmail.com>
2025-09-20 13:00:16 +0530

    changes

---
+++ 05-array.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "please enter uvalues of array "
+fruits=("apple" "goa" "orange")
+
+echo "the first element is " ${fruits[0]}
+
+echo "the second element is " ${fruits[1]}
+
+echo "the firstthirsd element is " ${fruits[3]}
+
