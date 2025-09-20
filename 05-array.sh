b2fd7bd1d405b48a342ee63590fe35518456231c
rajsekhar <rajsekhar9655@gmail.com>
2025-09-20 13:00:59 +0530

    changes

--- 05-array.sh
+++ 05-array.sh
@@ -7,5 +7,5 @@ echo "the first element is " ${fruits[0]}
 
 echo "the second element is " ${fruits[1]}
 
-echo "the firstthirsd element is " ${fruits[3]}
+echo "the firstthirsd element is " ${fruits[2]}
 
