2ffdfd316734d379f823c0a3b93f58f665110a43
rajsekhar <rajsekhar9655@gmail.com>
2025-09-20 12:25:08 +0530

    changes

---
+++ 04-unamepass.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "please enter username "
+read -s username
+echo "please enter password"
+read -s password
+
+echo "entered username - is $username and  entered password is $password "
+
+
