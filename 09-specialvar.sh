1d628ca57adb9e27e7d5d004fe5cc2e40f083c68
rajsekhar <rajsekhar9655@gmail.com>
2025-09-30 07:58:36 +0530

    changes

--- 09-specialvar.sh
+++ 09-specialvar.sh
@@ -2,3 +2,8 @@
 echo " to print all the varables passed to the script are $@"
 echo " the num of var pass to script are $# "
 echo " the name of the current script is $0" 
+echo " current working directory is $PWD "
+echo " current user home directory is $HOME "
+echo " PID is of the current script is $$ "
+sleep 100 &
+echo " PID is of the current script is $! "
