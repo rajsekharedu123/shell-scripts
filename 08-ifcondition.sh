644d261b84736b7a3538e6e84191f8a0dbb3fcc4
rajsekhar <rajsekhar9655@gmail.com>
2025-09-30 07:36:28 +0530

    changes

---
+++ 08-ifcondition.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+echo " if condition check"
+number=$1
+
+if [ $number -eq 20 ]
+then 
+    echo " the given number is equal to 20 "
+else 
+    echo " the given number is not equal to 20 "
+fi
+
