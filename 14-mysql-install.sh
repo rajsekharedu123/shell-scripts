8fa7c0d64cad3401bd7e70109cdfc6161cf1545c
rajsekhar <rajsekhar9655@gmail.com>
2025-09-30 13:06:49 +0530

    changes

---
+++ 14-mysql-install.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo " pls loggin with sudo "
+fi
+
+dnf installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed ....installing now"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+    echo "mysql is installed fail"
+    else
+        echo "mysql is sucess"
+    fi
+else
+echo "mysql is already installed ....installing now" 
+fi   
