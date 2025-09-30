ca7a31d32ebb8207194faa6d47d0104ca26c7f5e
rajsekhar <rajsekhar9655@gmail.com>
2025-09-30 12:48:31 +0530

    changes

---
+++ 13-git-install.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+echo " this is the start of the program "
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please loggin with the root or root previlages "
+    exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed , will install now"
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Git is installed , fail"
+        exit 1
+    else
+        echo "Git is installed success "
+
+else
+    echo "Git is already installed "
+
+fi
