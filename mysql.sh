b3dad3ea495737a7179be7ac6f3e333ac6f02d77
rajsekhar <rajsekhar9655@gmail.com>
2025-10-05 00:12:18 +0530

    changes

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
