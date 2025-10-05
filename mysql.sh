9a9d50dc1634f446e7a47a682d0b24bc78a779fc
rajsekhar <rajsekhar9655@gmail.com>
2025-10-05 08:56:31 +0530

    changes

--- mysql.sh
+++ mysql.sh
@@ -19,9 +19,9 @@ CHECK_ROOT(){
 }
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then echo -e "$2 $R FAILED $N" | tee -a &>>$LOG_FILE
+    then echo -e "$2 $R FAILED $N" | tee -a $LOG_FILE
     else
-         echo -e "$2 $G PASS $N"  | tee -a &>>$LOG_FILE
+         echo -e "$2 $G PASS $N"  | tee -a $LOG_FILE
     fi      
 }
 
