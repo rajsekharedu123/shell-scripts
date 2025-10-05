9318cbfb0c673bf419a4405d8d7f2bfa9f356a82
rajsekhar <rajsekhar9655@gmail.com>
2025-10-05 15:35:54 +0530

    changes

--- backendv1.sh
+++ backendv1.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date date +%Y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
