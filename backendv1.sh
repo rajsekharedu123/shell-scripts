3cbc59883e01f8f23612190baadb5cd24752171f
rajsekhar <rajsekhar9655@gmail.com>
2025-10-05 15:48:53 +0530

    changes

--- backendv1.sh
+++ backendv1.sh
@@ -35,5 +35,8 @@ echo -e "$0 $G script started executing $N"
 dnf module disable nodejs -y &>>LOG_FILE
 VALIDATE $? "disable nodejs" 
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>LOG_FILE
 VALIDATE $? "denable nodejs:20"
+
+dnf install nodejs -y &>>LOG_FILE
+VALIDATE $? "install nodejs"
